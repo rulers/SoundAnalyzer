5610074cb66cc10ad4446cfcfd9fb78de0b7898d
Masaaki Kurihara <rulers.jp@gmail.com>
2012-06-13 17:42:23 +0900

    comment add
    オリジナルのソース場所を追記

--- synthesizer.js
+++ synthesizer.js
@@ -1,4 +1,6 @@
 /*
+　* Folk　From : https://github.com/agektmr/Audio-Visualizer
+ * Original Souce : https://github.com/agektmr/Audio-Visualizer/blob/master/synthesizer.js
  * Sampling Rate: 44,100
  * Frequency:
  * Samples
